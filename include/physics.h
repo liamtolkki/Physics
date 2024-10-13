4b0649de38126f319cdbf919b0de1ed6b84b8beb
Liam Tolkkinen <liamtolkki@gmail.com>
2024-10-13 00:53:26 -0400

    added some useful constants to the physics header

--- include/physics.h
+++ include/physics.h
@@ -1,3 +1,16 @@
 #pragma once
 #include <mathlib/math.h>
 
+// useful constants:
+const double G = 6.67430e-11;                          // Gravitational constant
+const double g = 9.80665;                              // m/s²; acceleration due to gravity on earth
+const double c = 2.99792468e8;                         // m/s; speed of light in a vaccuum
+const double epsilon_0 = 8.8541878128e-12;             // F/m; the permittivity of free space
+const double mu_0 = 1.25663706143e-6;                  // F/A²; the permeability of free space
+const double electron_mass = 9.10938356e-31;           // mass of an electron
+const double proton_mass = 1.67262192369e-27;          // kg; mass of a proton
+const double neutron_mass = 1.67492749804e-27;         // kg; mass of a neutron
+const double earth_mass = 5.97219e24;                  // kg; mass of the earth
+const double earth_radius = 6.371e6;                   // m; radius of the earth
+const double elementary_charge = 1.602176634e-19;      // C; elementary charge (e)
+const double coulombs_constant = 8.9875517873681764e9; // N·m²/C²
